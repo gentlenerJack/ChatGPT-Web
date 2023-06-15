db17b28a61d63275e9d03353befaadcd83ad28b8
gordon.wang <>
2023-06-15 09:54:53 +0800

    增加多伦keys

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -176,7 +176,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "点击左下角[设置](/#/settings)按钮输入密码后解锁**免费使用**",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
